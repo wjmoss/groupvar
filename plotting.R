f1a9df00db17019332d1d939191230fd90e2ee3d
wjmoss <wujun3544@gmail.com>
2022-03-10 10:28:12 +0100

    draft 1.4

--- plotting.R
+++ plotting.R
@@ -1,9 +1,20 @@
 # set vc (vertex color) and vfc (vertex boudary color) as vector of string for different partition blocks
-plotDAG <- function(
+# if the input is a cpdag, it will plot a cpdag
+plotG <- function(
   dag,
-  part=1:ncol(dag),
+  part=NULL,
+  #part=1:ncol(dag),
   tcltk=FALSE,
   vfc="black",
   ...
-) ggm::plotGraph(dag, layout=igraph::layout.circle, tcltk=tcltk, vc=part, vfc=vfc, ...)
-
+){
+  mg <- dag
+  poslist <- which(lower.tri(dag) & dag == 1 & dag == t(dag))
+  for (pos in poslist){
+    #(i,j) entry
+    j <- ceiling(pos / ncol(mg))
+    i <- (pos - 1) %% ncol(mg) + 1
+    mg[i, j] <- mg[j, i] <- 10
+  }
+  ggm::plotGraph(mg, layout=igraph::layout.circle, tcltk=tcltk, vc=part, vfc=vfc, directed=F, ...)
+}
